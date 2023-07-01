181086d09032965a3373fc42361c9f83848a87a9
Егор Косяков <gript31@gmail.com>
2023-07-01 23:24:12 +0300

    Rewritten sender with fetch. Now, it really works!

--- sender.js
+++ sender.js
@@ -1,8 +1,28 @@
-async function postData(data,post_url){
-    let xhr = new XMLHttpRequest();  
-    xhr.open("POST",post_url,true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(data);
-};
+
+function postData(data,post_url){
+    //Пробуем отправить данные по ссылке
+    fetch(post_url,{
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' }
+    }).then((res)=>{
+        //проверяем результат отправки
+        if(res.ok){
+            return res;
+        }
+        //если не ок то ошибка
+        throw new Error("There is some kind of error in POST process!\n")
+    })
+    //выводим ответ
+    .then(res => res.text())
+    .then(text=>console.log(text))
+    //ловим ошибку
+    .catch((error)=>{
+        console.log(error);
+    })
+}
+
+
+
 
 module.exports = {postData};
